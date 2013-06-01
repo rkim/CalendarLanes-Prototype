37b6fd4893fb2499bf62f5a431d813b22b2b6b49
Roy Kim <kim.roy@gmail.com>
2013-06-01 16:25:29 -0700

    Reformatted CalendarLanes to use divs and added scroll axis
    1. Changed calendar layout elements from tables to divs.
    2. Modified dragscrollable library to be able to lock scrolling by x /y
    axes

--- dragscrollable.js
+++ dragscrollable.js
@@ -3,9 +3,14 @@
  * version: 1.0 (25-Jun-2009)
  * Copyright (c) 2009 Miquel Herrera
  *
+ *
  * Dual licensed under the MIT and GPL licenses:
  *   http://www.opensource.org/licenses/mit-license.php
  *   http://www.gnu.org/licenses/gpl.html
+ *
+ *
+ * Modified 26-Jul-2013 - Roy Kim
+ *   Add notes here...
  *
  */
 ;(function($){ // secure $ jQuery alias
@@ -17,94 +22,94 @@
  * respond to the drag events.
  * 
  * options properties:
- * ------------------------------------------------------------------------		
+ * ------------------------------------------------------------------------
  *  dragSelector         | jquery selector to apply to each wrapped element 
  *                       | to find which will be the dragging elements. 
  *                       | Defaults to '>:first' which is the first child of 
  *                       | scrollable element
- * ------------------------------------------------------------------------		
+ * ------------------------------------------------------------------------
  *  acceptPropagatedEvent| Will the dragging element accept propagated 
- *	                     | events? default is yes, a propagated mouse event 
- *	                     | on a inner element will be accepted and processed.
- *	                     | If set to false, only events originated on the
- *	                     | draggable elements will be processed.
+ *                       | events? default is yes, a propagated mouse event 
+ *                       | on a inner element will be accepted and processed.
+ *                       | If set to false, only events originated on the
+ *                       | draggable elements will be processed.
  * ------------------------------------------------------------------------
  *  preventDefault       | Prevents the event to propagate further effectivey
  *                       | dissabling other default actions. Defaults to true
+ * ------------------------------------------------------------------------
+ *  dragXAxis            | 
+ *                       | 
+ * ------------------------------------------------------------------------
+ *  dragYAxis            | 
+ *                       | 
  * ------------------------------------------------------------------------
  *  
  *  usage examples:
  *
  *  To add the scroll by drag to the element id=viewport when dragging its 
  *  first child accepting any propagated events
- *	$('#viewport').dragscrollable(); 
+ *  $('#viewport').dragscrollable(); 
  *
  *  To add the scroll by drag ability to any element div of class viewport
  *  when dragging its first descendant of class dragMe responding only to
  *  evcents originated on the '.dragMe' elements.
- *	$('div.viewport').dragscrollable({dragSelector:'.dragMe:first',
- *									  acceptPropagatedEvent: false});
+ *  $('div.viewport').dragscrollable({dragSelector:'.dragMe:first',
+ *                                    acceptPropagatedEvent: false});
  *
  *  Notice that some 'viewports' could be nested within others but events
  *  would not interfere as acceptPropagatedEvent is set to false.
- *		
+ *
  */
-$.fn.dragscrollable = function( options ){
-   
+$.fn.dragscrollable = function(options){
 	var settings = $.extend(
-		{   
+		{
 			dragSelector:'>:first',
 			acceptPropagatedEvent: true,
-            preventDefault: true
-		},options || {});
+			preventDefault: true,
+			dragXAxis: true,
+			dragYAxis: true
+		}, options || {});
 	 
-	
 	var dragscroll= {
 		mouseDownHandler : function(event) {
 			// mousedown, left click, check propagation
-			if (event.which!=1 ||
-				(!event.data.acceptPropagatedEvent && event.target != this)){ 
+			if (event.which != 1 || (!event.data.acceptPropagatedEvent && event.target != this)) { 
 				return false; 
 			}
-			
 			// Initial coordinates will be the last when dragging
 			event.data.lastCoord = {left: event.clientX, top: event.clientY}; 
-		
-			$.event.add( document, "mouseup", 
-						 dragscroll.mouseUpHandler, event.data );
-			$.event.add( document, "mousemove", 
-						 dragscroll.mouseMoveHandler, event.data );
+			$.event.add(document, "mouseup", dragscroll.mouseUpHandler, event.data);
+			$.event.add(document, "mousemove", dragscroll.mouseMoveHandler, event.data);
+			
 			if (event.data.preventDefault) {
-                event.preventDefault();
-                return false;
-            }
+				event.preventDefault();
+				return false;
+			}
 		},
 		mouseMoveHandler : function(event) { // User is dragging
 			// How much did the mouse move?
-			var delta = {left: (event.clientX - event.data.lastCoord.left),
-						 top: (event.clientY - event.data.lastCoord.top)};
+			var deltaX = (event.data.dragXAxis) ? event.clientX - event.data.lastCoord.left : 0;
+			var deltaY = (event.data.dragYAxis) ? event.clientY - event.data.lastCoord.top : 0;
+			var delta = {left: deltaX, top: deltaY};
 			
 			// Set the scroll position relative to what ever the scroll is now
-			event.data.scrollable.scrollLeft(
-							event.data.scrollable.scrollLeft() - delta.left);
-			event.data.scrollable.scrollTop(
-							event.data.scrollable.scrollTop() - delta.top);
+			event.data.scrollable.scrollLeft(event.data.scrollable.scrollLeft() - delta.left);
+			event.data.scrollable.scrollTop(event.data.scrollable.scrollTop() - delta.top);
 			
 			// Save where the cursor is
 			event.data.lastCoord={left: event.clientX, top: event.clientY}
 			if (event.data.preventDefault) {
-                event.preventDefault();
-                return false;
-            }
-
+				event.preventDefault();
+				return false;
+			}
 		},
 		mouseUpHandler : function(event) { // Stop scrolling
 			$.event.remove( document, "mousemove", dragscroll.mouseMoveHandler);
 			$.event.remove( document, "mouseup", dragscroll.mouseUpHandler);
 			if (event.data.preventDefault) {
-                event.preventDefault();
-                return false;
-            }
+				event.preventDefault();
+				return false;
+			}
 		}
 	}
 	
@@ -112,11 +117,13 @@ $.fn.dragscrollable = function( options ){
 	this.each(function() {
 		// closure object data for each scrollable element
 		var data = {scrollable : $(this),
-					acceptPropagatedEvent : settings.acceptPropagatedEvent,
-                    preventDefault : settings.preventDefault }
+			acceptPropagatedEvent : settings.acceptPropagatedEvent,
+			preventDefault : settings.preventDefault,
+			dragXAxis : settings.dragXAxis,
+			dragYAxis : settings.dragYAxis}
+	
 		// Set mouse initiating event on the desired descendant
-		$(this).find(settings.dragSelector).
-						bind('mousedown', data, dragscroll.mouseDownHandler);
+		$(this).find(settings.dragSelector).bind('mousedown', data, dragscroll.mouseDownHandler);
 	});
 }; //end plugin dragscrollable
 
